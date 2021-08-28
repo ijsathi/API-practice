2dec281ce21bfd79496d3bc4db53667cd6ddfde9
ijsathi <ijsathi2001rs@gmail.com>
2021-08-28 15:24:37 +0600

    comment

---
+++ comment.js
@@ -0,0 +1,22 @@
+const loadComment = () =>{
+    fetch('https://jsonplaceholder.typicode.com/comments')
+    .then(res => res.json())
+    .then(data => displayComment(data))
+}
+loadComment()
+const displayComment = data =>{
+    // console.log(data);
+    const comments = document.getElementById('comments')
+    // console.log(comments);
+    for (const comment of data){
+        const div = document.createElement('div')
+        const h3 = document.createElement('h3')
+        const p = document.createElement('p')
+        h3.innerText = `ID: ${comment.id}, Name: ${comment.name.slice(0,20)}, E-mail: ${comment.email}`
+        p.innerText = `${comment.body}`
+        div.appendChild(h3)
+        div.appendChild(p)
+
+        comments.appendChild(div)
+    }
+}
